2309bbcf47ae1b4c0de2261f89938ecb384cf2e7
Cris B <cristina.bardan@gmail.com>
2020-04-18 17:18:35 -0500

    added all coments to notebook

--- datamodel/schema.sql
+++ datamodel/schema.sql
@@ -1,6 +1,5 @@
-﻿-- Base on the exported file from QuickDBD: https://www.quickdatabasediagrams.com/
-
-----------------------
+﻿--------------------------------
+-- Based on the exported file from QuickDBD: https://www.quickdatabasediagrams.com/
 
 drop table if exists genre_anime;
 drop table if exists rating;
@@ -8,47 +7,52 @@ drop table if exists genre;
 drop table if exists source_site;
 drop table if exists anime;
 
-CREATE TABLE "anime" (
+create table "anime" (
     "id" serial primary key,
     "title" varchar,
     "episodes" int
 );
 
 
-CREATE TABLE "source_site" (
+create table "source_site" (
     "id" serial primary key,
     "source_name" varchar
 );
 
-CREATE TABLE "genre" (
+create table "genre" (
     "id" serial primary key,
     "genre_name" varchar
 );
 
-CREATE TABLE "rating" (
+create table "rating" (
     "id" serial primary key,
     "id_anime" int,
     "id_source" int,
     "rating" int,
-    FOREIGN KEY("id_anime")
-    REFERENCES "anime" ("id"),
-    FOREIGN KEY("id_source")
-    REFERENCES "source_site" ("id")
+    foreign key("id_anime")
+    references "anime" ("id"),
+    foreign key("id_source")
+    references "source_site" ("id")
 );
 
-CREATE TABLE "genre_anime" (
+create table "genre_anime" (
     "id_anime" int,
     "id_genre" int,
-    FOREIGN KEY("id_anime")
-    REFERENCES "anime" ("id"),
-    FOREIGN KEY("id_genre")
-    REFERENCES "genre" ("id")
+    foreign key("id_anime")
+    references "anime" ("id"),
+    foreign key("id_genre")
+    references "genre" ("id")
 );
+
+--------------------------------
+-- Added source_site values manually
 
 insert into source_site (source_name)
 values ('Crunchyroll'),
 		('MyAnimeList');
-		
+
+--------------------------------		
+-- Query ran to verify information
 
 select 
 	a.title, 
